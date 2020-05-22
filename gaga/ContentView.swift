db7eda8933dfb48e41762f6fd55c30cab03a15fa
NTOU <fus22803281@gmail.com>
2020-05-22 13:06:41 +0800

    Initial Commit

---
+++ gaga/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gaga
+//
+//  Created by User17 on 2020/5/22.
+//  Copyright © 2020 NTOU. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
